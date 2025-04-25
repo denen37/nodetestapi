c240d42509657c53cd30a6ff1bf9eed7eff60029
Denen <awardenen@gmail.com>
2025-04-25 20:05:17 +0100

    Update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+require('dotenv').config({ path: './config/.env' });
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+const env = process.env;
+
 
 app.get('/', (req, res) => {
     res.send('Welcome, Your app is working!')
 })
 
+console.log('PORT:', env.PORT)
+console.log('HOST:', env.HOST)
+console.log('APP:', env.APP_NAME)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
